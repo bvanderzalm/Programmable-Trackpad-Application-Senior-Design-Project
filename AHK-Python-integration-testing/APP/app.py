ff9bb96699fc6e008fb9b875031a58d86b908c31
Bradley Vanderzalm <bvan544@gmail.com>
2023-01-29 23:22:45 -0500

    Added more macros and code cleanup

--- AHK-Python-integration-testing/APP/app.py
+++ AHK-Python-integration-testing/APP/app.py
@@ -9,7 +9,13 @@ class App(customtkinter.CTk):
     APP_NAME = "Programmable MacroPad"
     WIDTH = 800
     HEIGHT = 500
-    MACRO_LIST: list[str] = ["Google search selected text", "Open UCF site", "Open Notepad", "Move up a folder"]
+    MACRO_LIST: list[str] = [
+        "Google Search Selected Text", "Open Website", "Open Application", "Move up a Folder",
+        "Copy HEX color code to clipboard", "Open Command Prompt in current folder", "Run command in current folder",
+        "Open Command Prompt at a Favorite Folder", "Run Command at a Favorite Folder", 
+        "Open File Explorer at a Favorite Folder", "Volume Up", "Volume Down", "Volume Mute", "Play/Pause Media","Empty Recycle Bin", 
+        "Insert preset message",
+        ]
     KEY1: str = ''
     KEY2: str = ''
     KEY3: str = ''
@@ -44,40 +50,34 @@ class App(customtkinter.CTk):
         self.stopAhkButton = customtkinter.CTkButton(master=self.sidebar,text="Stop AHK", command=self.stop_ahk)
         self.stopAhkButton.grid(pady=(20, 0), padx=(20, 20), row=1, column=0)
 
-        self.macro_label = customtkinter.CTkLabel(self.sidebar, text="Select Macros:", anchor="w")
-        self.macro_label.grid(row=3, column=0, padx=(20, 20), pady=(20, 0))
-        self.macro_option_menu = customtkinter.CTkOptionMenu(self.sidebar, values=["F1 - Google search selected text", "F2 - Open UCF site", "F3 - Open Notepad", "F4 - Move up a folder"],
-                                                                       command=self.edit_macro)
-        self.macro_option_menu.grid(row=4, column=0, padx=(20, 20), pady=(10, 0))
-
         self.appearanceModeLabel = customtkinter.CTkLabel(self.sidebar, text="Appearance Mode:", anchor="w")
         self.appearanceModeLabel.grid(row=5, column=0, padx=(20, 20), pady=(20, 0))
         self.appearanceModeMenu = customtkinter.CTkOptionMenu(self.sidebar, values=["Light", "Dark", "System"], command=self.change_appearance_mode)
         self.appearanceModeMenu.grid(row=6, column=0, padx=(20, 20), pady=(10, 20))
 
         # -- Setup Home component --
-
+        
         self.searchBar = customtkinter.CTkEntry(master=self.home, placeholder_text="Search Preset", width=300)
-        self.searchBar.grid(row=0, column=0, sticky="we", padx=(12,0), pady=(12,12))
+        self.searchBar.grid(row=0, column=0, padx=(12,0), pady=(12,12))
         self.searchBar.bind("<Return>", self.search_preset)
 
         self.searchButton = customtkinter.CTkButton(master=self.home, text="Search", width=90, command=self.search_preset)
-        self.searchButton.grid(row=0, column=1, sticky="w", padx=(12,0), pady=(12,12))
+        self.searchButton.grid(row=0, column=1, padx=(12,0), pady=(12,12))
 
-        self.keyOneLabel = customtkinter.CTkLabel(self.home, text="First function key:", anchor="w")
-        self.keyTwoLabel = customtkinter.CTkLabel(self.home, text="Second function key:", anchor="w")
-        self.keyThreeLabel = customtkinter.CTkLabel(self.home, text="Third function key:", anchor="w")
-        self.keyFourLabel = customtkinter.CTkLabel(self.home, text="Fourth function key:", anchor="w")
+        self.keyOneLabel = customtkinter.CTkLabel(master=self.home, text="First function key:", anchor="w")
+        self.keyTwoLabel = customtkinter.CTkLabel(master=self.home, text="Second function key:", anchor="w")
+        self.keyThreeLabel = customtkinter.CTkLabel(master=self.home, text="Third function key:", anchor="w")
+        self.keyFourLabel = customtkinter.CTkLabel(master=self.home, text="Fourth function key:", anchor="w")
 
         self.keyOneLabel.grid(row=1, column=0, sticky="w", padx=(10, 10), pady=(0, 0))
         self.keyTwoLabel.grid(row=2, column=0, sticky="w", padx=(10, 10), pady=(10, 0))
         self.keyThreeLabel.grid(row=3, column=0, sticky="w", padx=(10, 10), pady=(10, 0))
         self.keyFourLabel.grid(row=4, column=0, sticky="w", padx=(10, 10), pady=(10, 0))
 
-        self.keyOneOptionMenu = customtkinter.CTkOptionMenu(self.home, values=App.MACRO_LIST, command=self.update_key1)
-        self.keyTwoOptionMenu = customtkinter.CTkOptionMenu(self.home, values=App.MACRO_LIST, command=self.update_key2)
-        self.keyThreeOptionMenu = customtkinter.CTkOptionMenu(self.home, values=App.MACRO_LIST, command=self.update_key3)
-        self.keyFourOptionMenu = customtkinter.CTkOptionMenu(self.home, values=App.MACRO_LIST, command=self.update_key4)
+        self.keyOneOptionMenu = customtkinter.CTkOptionMenu(master=self.home, values=App.MACRO_LIST, command=self.update_key1, dynamic_resizing=False, width=200)
+        self.keyTwoOptionMenu = customtkinter.CTkOptionMenu(master=self.home, values=App.MACRO_LIST, command=self.update_key2, dynamic_resizing=False, width=200)
+        self.keyThreeOptionMenu = customtkinter.CTkOptionMenu(master=self.home, values=App.MACRO_LIST, command=self.update_key3, dynamic_resizing=False, width=200)
+        self.keyFourOptionMenu = customtkinter.CTkOptionMenu(master=self.home, values=App.MACRO_LIST, command=self.update_key4, dynamic_resizing=False, width=200)
 
         self.keyOneOptionMenu.grid(row=1, column=1, padx=(10,10), pady=(10, 0))
         self.keyTwoOptionMenu.grid(row=2, column=1, padx=(10,10), pady=(10, 0))
@@ -108,14 +108,47 @@ class App(customtkinter.CTk):
             functionKey: str = "F" + str(counter)
             # Add AutoHotKey remapping functions to text file
             match key:
-                case "Google search selected text":
+                case "Google Search Selected Text":
                     f.write("{\n" + functionKey + "::\n\tSend, ^c\n\tSleep 50\n\tRun, https://www.google.com/search?q=%clipboard%\n\tReturn\n}\n\n")
-                case "Open UCF site":
-                    f.write(functionKey + '::Run, Chrome.exe "https://www.ucf.edu"\n\n')
-                case "Open Notepad":
-                    f.write(functionKey + "::Run Notepad\n\n")
-                case "Move up a folder":
+                case "Open Website":
+                    website: str = "https://www.ucf.edu"
+                    f.write(functionKey + "::Run, " + website + "\n\n")
+                case "Open Application":
+                    appProcess: str = "Notepad"
+                    f.write(functionKey + "::Run " + appProcess + "\n\n")
+                case "Move up a Folder":
                     f.write(functionKey + "::Send !{Up}\n\n")
+                case "Copy HEX color code to clipboard":
+                    f.write(functionKey + "::\n{\n\tMouseGetPos, MouseX, MouseY\n\tPixelGetColor, color, %MouseX%, %MouseY%, RGB\n\tStringLower, color, color\n\tclipboard := SubStr(color, 3)\n\tReturn\n}\n\n")
+                case "Open Command Prompt in current folder":
+                    f.write(functionKey + "::\n{\n\tSend, !d\n\tSend,^c\n\tSleep 50\n\tRun cmd, %clipboard%\n\tReturn\n}\n\n")
+                case "Run command in current folder":
+                    command: str = "git status"
+                    f.write(functionKey + "::\n{\n\tSend, !d\n\tSend,^c\n\tSleep 50\n\tRun cmd, %clipboard%\n\tSleep 100\n\tSend, " + command + "\n\tSleep 100\n\tSend, {Enter}\n\tReturn\n}\n\n")
+                case "Open Command Prompt at a Favorite Folder":
+                    folderLocation: str = "C:\\Users\\bvan5\\Desktop\\SeniorDesign"
+                    f.write(functionKey + "::Run cmd, " + folderLocation + "\n\n")
+                case "Run Command at a Favorite Folder":
+                    folderLocation: str = "C:\\Users\\bvan5\\Desktop\\SeniorDesign"
+                    command: str = "git status"
+                    f.write(functionKey + '::\n{\n\tRun cmd, ' + folderLocation + '\n\tSleep 100\n\tSend, ' + command + '\n\tSleep 100\n\tSend, {Enter}\n\tReturn\n}\n\n')
+                case "Open File Explorer at a Favorite Folder":
+                    folderLocation: str = "C:\\Users\\bvan5\\Desktop\\SeniorDesign"
+                    f.write(functionKey + "::Run " + folderLocation + "\n\n")
+                case "Volume Up":
+                    f.write(functionKey + "::Volume_Up\n\n")
+                case "Volume Down":
+                    f.write(functionKey + "::Volume_Down\n\n")
+                case "Volume Mute":
+                    f.write(functionKey + "::Volume_Mute\n\n")
+                case "Play/Pause Media":
+                    f.write(functionKey + "::Media_Play_Pause\n\n")
+                case "Empty Recycle Bin":
+                    f.write(functionKey + "::FileRecycleEmpty\n\n")
+                case "Insert preset message":
+                    message: str = "this will show up whenever the user is typing"
+                    f.write(functionKey + "::Send " + message + "\n\n")
+
             counter = counter + 1
         
         # Save newly created .ahk file
@@ -148,16 +181,6 @@ class App(customtkinter.CTk):
 
     def change_appearance_mode(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
-
-    def edit_macro(self, macro: str):
-        if macro == "F1 - Google search selected text":
-            print('Edit F1 Macro')
-        elif macro == "F2 - Open UCF site":
-            print('Edit F2 Macro')
-        elif macro == "F3 - Open Notepad":
-            print('Edit F3 Macro')
-        elif macro == "F4 - Move up a folder":
-            print('Edit F4 Macro')
 
     def on_closing(self, event=0):
         self.destroy()
--- AHK-Python-integration-testing/APP/program-files/macro-pad.ahk
+++
@@ -1,4 +0,0 @@
-F1::Run Notepad
-
-F2::Run, Chrome.exe "https://www.ucf.edu"
-
